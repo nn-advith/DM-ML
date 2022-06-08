230c72f11fa279a27a79660f5a5cad70117a1e57
N N Advith <nnadvith08042001@gmail.com>
2022-06-08 19:35:54 +0530

    sfhjgv

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"haha12345advith@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo “
+[server]
+headless = true
+enableCORS=false
+port = $PORT
+“ > ~/.streamlit/config.toml
